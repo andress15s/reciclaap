6d5183b9071fbc8aac5bc5fb987102c69be82662
JorgeM28 <jorge2224764@correo.uis.edu.co>
2023-06-10 19:44:58 -0500

    Herencia

---
+++ EDICION/modelo/Carton.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Carton extends Material
+{
+    public String Descripcion() 
+    {
+        return "Nombre: Carton\n Descripcion: El carton es un material ampliamente utilizado y facilmente reciclable.\n Para reciclar el carton, sigue los siguientes pasos:\n 1. Asegurate de eliminar cualquier cinta adhesiva, etiquetas o elementos no reciclables del carton.\n 2. Si es posible, aplasta o pliega el carton para reducir su volumen y facilitar su transporte.\n 3. Coloca el carton en el contenedor de reciclaje especifico para papel y carton.\n 4. Si no tienes acceso a un contenedor de reciclaje, puedes llevar el carton a un centro de reciclaje cercano.\n 5. Recuerda seguir las instrucciones locales de reciclaje y contribuir al cuidado del medio ambiente.\n Gracias por reciclar!";
+    }
+    
+ 
+}
--- EDICION/modelo/Material.java
+++ EDICION/modelo/Material.java
@@ -2,51 +2,9 @@ package modelo;
 
 public class Material 
 {
-
-    //atributos
-    private String nombre;
-    private String descripcion;
-    private String tipo;
-
-    //constructor
-    public Material(String nombre, String descripcion, String tipo) 
+    public String Descripcion() 
     {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.tipo = tipo;
+        return "Material";
     }
-
-    //getters y setters
-    public String getNombre() 
-    {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) 
-    {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() 
-    {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) 
-    {
-        this.descripcion = descripcion;
-    }
-
-    public String getTipo() 
-    {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) 
-    {
-        this.tipo = tipo;
-    }
-
-    
 
 }
